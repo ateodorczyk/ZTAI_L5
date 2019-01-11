cb464356537d25ef4a27c1cea6fc995cc824fbb7
28344 <miskoo96@gmail.com>
2019-01-11 18:06:59 +0100

    lab 5

---
+++ src/main/java/com/example/ztai5/Ztai5Application.java
@@ -0,0 +1,14 @@
+package com.example.ztai5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ztai5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ztai5Application.class, args);
+	}
+
+}
+
